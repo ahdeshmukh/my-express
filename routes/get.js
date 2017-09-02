4345f2d5eb49fc72c0feafc4d442c2d89ea29278
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-09-02 11:28:08 -0400

    route with optional paramter

--- routes/get.js
+++ routes/get.js
@@ -2,13 +2,17 @@
 	var express = require('express');
 	var router = express.Router();
 
-	router.get('/hello', function(req, res){
-	   res.send('GET route on things- Hello from get.js.');
+	router.get('/hello/:id?', function(req, res){
+		var return_text = 'GET route on things- Hello from get.js.';
+		if(req.params.id) {
+			return_text += 'The id you specified is ' + req.params.id;
+		}
+	   res.send(return_text);
 	});
 
-	router.get('/hello/:id', function(req, res){
+	/*router.get('/hello/:id', function(req, res){
 	   res.send('GET route on things- Hello from get.js. The id you specified is ' + req.params.id);
-	});
+	});*/
 
 	//export this router to use in our server.js if necessary
 	module.exports = router;
