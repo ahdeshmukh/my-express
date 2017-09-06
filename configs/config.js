026e65d4903f09dfa37a3ffa2178b444ecc3d216
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-09-06 18:34:25 -0400

    adding info for mlab heroku app

--- configs/config.js
+++ configs/config.js
@@ -1,15 +1,24 @@
 (function(){
 	var config = {
 		env: 'DEV123XYZ',
-		mongodb: {
+		/*mongodb: {
 			'driver': 'mongodb',
 			'host': 'localhost',
 			'port': '27017',
 			'database': 'mean_stack',
 			'user':'',
 			'password': ''
+		}*/
+		mongodb: {
+			'driver': 'mongodb',
+			'host': 'ds129394.mlab.com',
+			'port': '29394',
+			'database': 'heroku_l9h0958w',
+			'user':'heroku_l9h0958w',
+			'password': 'amm7qffiikm267ec82rbq4mpr9'
 		}
 	};
 	
 	module.exports = config;
 })();
+// mongodb://heroku_l9h0958w:amm7qffiikm267ec82rbq4mpr9@ds129394.mlab.com:29394/heroku_l9h0958w
