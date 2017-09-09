a805d33633e336ba56c7f23e78e67492bfd4bba5
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-09-09 11:33:24 -0400

    catching mongodb connection error

--- server.js
+++ server.js
@@ -4,11 +4,19 @@
 	const bodyParser = require('body-parser');
 	const mongoose = require('mongoose');
 
+	const utility_service = require('./services/utility.service');
+
 	const mongoose_service = require('./services/mongoose.service');
 	const mongo_connection_string = mongoose_service.getConnectionString();
 
-	mongoose.connect(mongo_connection_string, {useMongoClient: true});
+	mongoose.connect(mongo_connection_string, {useMongoClient: true, reconnectTries: 3});
 	var db = mongoose.connection;
+	var mongoose_connection_success = true;
+	var mongoose_error = null;
+	mongoose.connection.on('error',function (err, res) {  
+		mongoose_error = utility_service.returnError(err);
+		mongoose_connection_success = false;
+	});
 	
 	// home page
 	app.use(express.static('public')) // use public folder for static file
@@ -23,6 +31,10 @@
 	var utility = require('./services/utility.service');
 	
 	app.use(function(req, res, next){
+		if(!mongoose_connection_success) {
+			res.json(utility_service.returnResult(mongoose_error));
+			return;
+		}
 		req.first_middle_ware = 'STARTINGXX -';
 		if(req.url == '/restricted') {
 			//res.send('Restricted route for ' + req.method);
@@ -37,6 +49,6 @@
 	app.use('/', put_routes);
 	app.use('/', delete_routes);
 
-	app.listen(process.env.PORT || 8080, () => console.log('All is ok'))
+	app.listen(process.env.PORT || 4000, () => console.log('All is ok'))
 	
 })();
