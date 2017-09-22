841b74adfe4d36e65cf500fccb1e73f41eefc660
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2017-09-22 16:49:20 -0400

    putting mongoose calls in try catch

--- models/user.model.js
+++ models/user.model.js
@@ -38,19 +38,35 @@
     var User = module.exports = mongoose.model('User', userSchema);
 
     module.exports.getUsers = function(callback, limit) {
-        User.find(callback).limit(limit);
+        try {
+            User.find({},{"password": 0},callback).limit(limit); // not returning the password
+        } catch(err) {
+            throw err;
+        }
     };
 
     module.exports.getUserById = function(id, callback) {
-        User.findById(id, callback);
+        try {
+            User.findById(id, callback);
+        } catch(err) {
+            throw err;
+        }
     };
 
     module.exports.addUser = function(user, callback) {
-        User.create(user, callback);
+        try {
+            User.create(user, callback);
+        } catch(err) {
+            throw err;
+        }
     };
 
     module.exports.updateUser = function(query, user, callback) {
-        User.update(query, user, callback);
+        try {
+            User.update(query, user, callback);
+        } catch(err) {
+            throw err;
+        }
     }
 
     module.exports.addTask = function(query, task, callback) {
@@ -58,30 +74,47 @@
 		task_obj.name = task;
 		task_obj.status = 'new';
         task_obj.created_time = Date.now();//dateFormat(now, "isoDateTime", true);//Date();
-        User.update(query, {$push: {"tasks": task_obj}}, callback);
+        try {
+            User.update(query, {$push: {"tasks": task_obj}}, callback);
+        } catch(err) {
+            throw err;
+        }
     }
 
     module.exports.getUserByEmail = function(email, callback) {
         let query = {"email": email};
-        User.findOne(query, callback);
-        // User.findOne(query, 'email password', callback); // to only return email and password
+        try {
+            User.findOne(query, callback);
+            // User.findOne(query, 'email password', callback); // to only return email and password
+        } catch(err) {
+            throw err;
+        }
     }
 
     module.exports.getUsersTasksCountByStatus = function(user_id, callback) {
         let user_id_obj = mongoose.Types.ObjectId(user_id);
-        User.aggregate([
-            {$match: {_id: user_id_obj, active: true}},
-            {$unwind: "$tasks"},
-            {$group: {_id: "$tasks.status", num_tasks: {$sum: 1}}},
-            {$project:{_id: 0, task_name:"$_id", num_tasks: 1}}
-        ], callback);
+        try{
+            User.aggregate([
+                {$match: {_id: user_id_obj, active: true}},
+                {$unwind: "$tasks"},
+                {$group: {_id: "$tasks.status", num_tasks: {$sum: 1}}},
+                {$project:{_id: 0, task_name:"$_id", num_tasks: 1}}
+            ], callback);
+        } catch(err) {
+            throw err;
+        }
     }
 
     module.exports.updateTask = function(user_id, task, callback) {
         let user_id_obj = mongoose.Types.ObjectId(user_id);
         let query = {_id:user_id_obj, "tasks.name":task.name, "tasks.created_time":task.created_time};
         let update = {$set:{"tasks.$.status":task.status}};
-        User.update(query, update, callback)
+        try {
+            User.update(query, update, callback)
+        } catch(err) {
+            throw err;
+        }
+        
     }
 
 })();
